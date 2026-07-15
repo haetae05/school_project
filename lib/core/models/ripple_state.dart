05bf8102a5f6f3f6ad3b09a15d6ac3947bcc6907
Gureum_Haetae <startup0025@gmail.com>
2026-07-15 10:11:02 +0900

    Wipe repository clean for React Native pivot

--- lib/core/models/ripple_state.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-enum RippleState { calm, walking, busy, danger }
-
-final rippleStateProvider = StateProvider<RippleState>((ref) => RippleState.calm);
